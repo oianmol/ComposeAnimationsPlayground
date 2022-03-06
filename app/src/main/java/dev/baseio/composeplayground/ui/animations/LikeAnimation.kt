1a75c9f8f885871603465af2b02536f033b54661
Anmol Verma <anmol.verma4@gmail.com>
2022-03-06 20:58:03 +0530

    added like button animation

--- app/src/main/java/dev/baseio/composeplayground/ui/animations/LikeAnimation.kt
+++ app/src/main/java/dev/baseio/composeplayground/ui/animations/LikeAnimation.kt
@@ -3,6 +3,7 @@ package dev.baseio.composeplayground.ui.animations
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -22,10 +23,10 @@ import kotlinx.coroutines.launch
 @Composable
 fun LikeAnimation(modifier: Modifier) {
 
-  var ringScale = remember {
+  val ringScale = remember {
     Animatable(0f)
   }
-  var ringOpacity = remember {
+  val ringOpacity = remember {
     Animatable(1f)
   }
   val imageScale = remember {
@@ -40,11 +41,11 @@ fun LikeAnimation(modifier: Modifier) {
     Animatable(0f)
   }
 
-  var orbScale = remember {
+  val orbScale = remember {
     Animatable(0f)
   }
 
-  var color by remember {
+  val color = remember {
     mutableStateOf(Color.Gray)
   }
 
@@ -57,36 +58,52 @@ fun LikeAnimation(modifier: Modifier) {
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
       Button(onClick = {
-
         coroutineScope.launch {
-          orbOffset.animateTo(0f)
-          ringOpacity.animateTo(1f)
-          ringScale.animateTo(0f)
-          imageScale.animateTo(0.6f, tween(300, easing = LinearEasing))
-          imageRotation.animateTo(-30f, tween(300, easing = LinearEasing))
+          ringScale.snapTo(0f)
+          ringOpacity.snapTo(1f)
+          imageScale.snapTo(1f)
+          imageRotation.snapTo(0f)
+          orbOffset.snapTo(0f)
+          orbScale.snapTo(0f)
+          color.value = Color.Gray
         }
 
-        coroutineScope.launch {
-          delay(300)
-          orbOffset.animateTo(orbOffset.value.minus(50f), tween(600))
-          color = Color.Blue
-          orbScale.animateTo(1f)
-          ringScale.animateTo(1f)
-          imageScale.animateTo(1f)
-          ringOpacity.animateTo(0f)
-          imageRotation.animateTo(0f)
-        }
 
-        coroutineScope.launch {
-          delay(600)
-          orbScale.animateTo(0f, tween(300))
-        }
       }) {
-        Text(text = "Repeat Animation!")
+        Text(text = "Reset Animation!")
       }
       Spacer(modifier = Modifier.height(28.dp))
       Column(
         Modifier
+          .clickable {
+            coroutineScope.launch {
+              orbOffset.animateTo(0f)
+              ringOpacity.animateTo(1f)
+              ringScale.animateTo(0f)
+              imageScale.animateTo(0.6f, tween(300, easing = LinearEasing))
+              imageRotation.animateTo(-30f, tween(300, easing = LinearEasing))
+            }
+
+            coroutineScope.launch {
+              delay(300)
+              coroutineScope.launch {
+                orbOffset.animateTo(orbOffset.value.minus(50f), tween(600))
+              }
+              color.value = green
+              coroutineScope.launch {
+                orbScale.animateTo(1f)
+                ringScale.animateTo(1f)
+                imageScale.animateTo(1f)
+                ringOpacity.animateTo(0f)
+                imageRotation.animateTo(0f)
+              }
+            }
+
+            coroutineScope.launch {
+              delay(600)
+              orbScale.animateTo(0f, tween(300))
+            }
+          }
           .padding(4.dp)
       ) {
 
@@ -102,12 +119,13 @@ fun LikeAnimation(modifier: Modifier) {
           for (item in 0 until 8) {
             Canvas(
               modifier = Modifier
+                .align(Alignment.Center)
                 .rotate(item.times(45f))
                 .offset(x = 0.dp, y = (if (item % 2 == 0) offsetEven else offsetOdd.dp))
                 .scale(orbScale.value),
               onDraw = {
                 drawCircle(
-                  color = Color.Blue,
+                  color = yellow,
                   alpha = if (item % 2 == 0) 1f else 0.5f,
                   radius = if (item % 2 == 0) 15f else 5f
                 )
@@ -117,6 +135,7 @@ fun LikeAnimation(modifier: Modifier) {
 
           Canvas(modifier = Modifier
             .size(100.dp)
+            .align(Alignment.Center)
             .scale(ringScale.value), onDraw = {
             drawCircle(
               color = Color.Gray.copy(alpha = ringOpacity.value),
@@ -127,7 +146,8 @@ fun LikeAnimation(modifier: Modifier) {
           Icon(
             painter = painterResource(id = R.drawable.ic_thumb),
             contentDescription = null,
-            tint = color, modifier = Modifier
+            tint = color.value, modifier = Modifier
+              .align(Alignment.Center)
               .size(50.dp)
               .scale(imageScale.value)
               .rotate(imageRotation.value)
