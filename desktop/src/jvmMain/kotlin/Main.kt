33a8756f8708461117abcb4d06b70049d3637713
Anmol Verma <anmol.verma4@gmail.com>
2023-08-03 19:15:07 +0530

    update: rememberWindowSize for Desktop

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,6 +1,9 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.runtime.*
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowSize
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import dev.baseio.common.App
@@ -11,7 +14,7 @@ import kotlinx.coroutines.flow.onEach
 
 
 fun main() = application {
-    val windowState = rememberWindowState()
+    val windowState = rememberWindowState(size = DpSize(1024.dp,768.dp))
 
     Window(onCloseRequest = ::exitApplication, state = windowState) {
         var rememberedComposeWindow by remember(this.window) {
